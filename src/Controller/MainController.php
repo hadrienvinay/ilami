35c86e6d3fed018f2dae6e7c2a1715d1643b918b
Hadrien Vinay <hadrien.vinay@yahoo.fr>
2019-07-12 17:55:46 +0200

    First one

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/MainController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     * @throws \Exception
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('base.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
